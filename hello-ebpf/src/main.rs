53e77c3376030a3fed0daa4892fe97ba9c84470d
albertlockett <albert.lockett@sonraisecurity.com>
2021-12-20 19:27:38 -0500

    made code

---
+++ hello-ebpf/src/main.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+use aya_bpf::{
+    macros::kprobe,
+    programs::ProbeContext,
+};
+use aya_log_ebpf::info;
+
+#[kprobe(name="hello")]
+pub fn hello(ctx: ProbeContext) -> u32 {
+    info!(&ctx, "Hello world");
+    match unsafe { try_hello(ctx) } {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+unsafe fn try_hello(ctx: ProbeContext) -> Result<u32, u32> {
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
